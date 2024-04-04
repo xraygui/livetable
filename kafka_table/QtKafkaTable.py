75c4fbae687c20ff657fb9ce51fb615da68bfa69
Charles Titus <ctitus@bnl.gov>
2024-04-03 23:07:14 -0400

    kafka table tab actually working, with some jank about monospace fonts that probably can go away

--- kafka_table/QtKafkaTable.py
+++ kafka_table/QtKafkaTable.py
@@ -1,187 +1,18 @@
 from qtpy.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout, QMainWindow
 from qtpy.QtCore import QThread, Slot, Signal, QObject, Qt, QTimer
+from qtpy.QtGui import QFontInfo, QFont
 import argparse
 import queue
 import time
 from .kafka_table import qt_kafka_table
+
 from bluesky_widgets.qt.run_engine_client import QtReConsoleMonitor
-#from .QtReConsoleMonitor import QtReConsoleMonitor
+
+# from .QtReConsoleMonitor import QtReConsoleMonitor
 import sys
 
 
-class LiveTableWorker(QObject):
-    finished = Signal()
-    lineReady = Signal(str)
-
-    def __init__(
-        self, beamline_acronym, config_file, topic_string="bluesky.runengine.documents"
-    ):
-        super().__init__()
-        self.beamline_acronym = beamline_acronym
-        self.config_file = config_file
-        self.topic_string = topic_string
-        self._polling = True
-        self.destroyed.connect(lambda: print("LiveTableWorker Destroyed Signal"))
-
-    @Slot()
-    def run(self):
-        print("Running KafkaTable")
-        kafka_dispatcher = qt_kafka_table(
-            self.beamline_acronym,
-            self.config_file,
-            self.topic_string,
-            self.lineEmitter,
-        )
-        kafka_dispatcher.setParent(self)
-        kafka_dispatcher.start(self.keepPolling)
-        print("Done with kafka_table")
-        self.finished.emit()
-
-    def lineEmitter(self, line):
-        self.lineReady.emit(line)
-
-    def stop(self):
-        self._polling = False
-
-    def keepPolling(self):
-        print(f"Keep Polling called! {self._polling}")
-        return self._polling
-
-    @Slot()
-    def aboutToQuit(self):
-        print("About To Quit")
-        self.stop()
-
-
 class LiveTableModel(QWidget):
-    def __init__(
-        self,
-        beamline_acronym,
-        config_file,
-        topic_string="bluesky.runengine.documents",
-        parent=None,
-    ):
-        super().__init__(parent)
-        self.thread = QThread()
-        self.msg_queue = queue.Queue()
-        self.worker = LiveTableWorker(beamline_acronym, config_file, topic_string)
-
-        self.destroyed.connect(lambda: self.stop_console_output_monitoring)
-        self.worker.moveToThread(self.thread)
-        self.worker.lineReady.connect(self.msg_queue.put)
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.start()
-
-    def start_console_output_monitoring(self):
-        self._stop_console_monitor = False
-        self.thread.quit()
-        self.thread.wait()
-
-        # self._rco = ReceiveConsoleOutput(zmq_subscribe_addr=self._zmq_info_addr, timeout=200)
-
-    def stop_console_output_monitoring(self):
-        print("Stop Console Monitoring")
-        self._stop_console_monitor = True
-        self.worker.stop()
-
-    # def console_monitoring_thread(self, *, callback):
-    def console_monitoring_thread(self):
-        while True:
-            print("Monitoring")
-            try:
-                msg = self.msg_queue.get(timeout=0.2)
-                msg = msg.rstrip("\n") + "\n"
-                msgtime = time.time()
-                return msgtime, msg
-
-            except queue.Empty:
-                pass
-            except Exception as ex:
-                print(f"Exception occurred: {ex}")
-
-            if self._stop_console_monitor:
-                print("Stop monitoring!")
-                break
-
-class LiveTableModel2(QWidget):
-    def __init__(
-        self,
-        beamline_acronym,
-        config_file,
-        topic_string="bluesky.runengine.documents",
-        parent=None,
-    ):
-        super().__init__(parent)
-        self.setAttribute(Qt.WA_DeleteOnClose)
-        self.msg_queue = queue.Queue()
-        #self.qtDispatcher = qt_kafka_table(beamline_acronym, config_file, topic_string, self.msg_queue.put)
-        #self.qtDispatcher.setParent(self)
-        label = QLabel("Test")
-        vbox = QVBoxLayout()
-        vbox.addWidget(label)
-        self.setLayout(vbox)
-        self.destroyed.connect(lambda: print("Destroying LiveTableModel2"))
-        self.start_console_output_monitoring()
-
-    @Slot()
-    def closeEvent(self, event):
-        print("Close event LiveTableModel2")
-
-    def start_console_output_monitoring(self):
-        print("Start Console Output Monitoring")
-        self._stop_console_monitor = False
-        self.console_monitoring_thread()
-        QTimer.singleShot(195, self.console_monitoring_thread)
-        #self.qtDispatcher.start(continue_polling=self.continue_polling)
-        # self._rco = ReceiveConsoleOutput(zmq_subscribe_addr=self._zmq_info_addr, timeout=200)
-
-    def stop_console_output_monitoring(self):
-        print("Stop Console Monitoring")
-        self._stop_console_monitor = True
-
-    def continue_polling(self):
-        return not self._stop_console_monitor
-
-    # def console_monitoring_thread(self, *, callback):
-    def console_monitoring_thread(self):
-        while True:
-            print("Monitoring")
-            try:
-                msg = self.msg_queue.get(timeout=0.2)
-                msg = msg.rstrip("\n") + "\n"
-                msgtime = time.time()
-                QTimer.singleShot(195, self.console_monitoring_thread)
-                return msgtime, msg
-
-            except queue.Empty:
-                pass
-            except Exception as ex:
-                print(f"Exception occurred: {ex}")
-
-            if self._stop_console_monitor:
-                print("Stop monitoring!")
-                break
-
-class TestWorker(QObject):
-    finished = Signal()
-
-    @Slot()
-    def run(self):
-        self._go = True
-        QTimer.singleShot(1000, self.hello)
-
-    def hello(self):
-        print("Hello!")
-        if self._go:
-            QTimer.singleShot(1000, self.hello)
-        else:
-            self.finished.emit()
-
-
-class LiveTableModel3(QWidget):
     def __init__(
         self,
         beamline_acronym,
@@ -208,7 +39,7 @@ class LiveTableModel3(QWidget):
 
     def newMsg(self, msg):
         self.msg_queue.put(msg)
-        print(msg)
+        # print(msg)
 
     def start_console_output_monitoring(self):
         print("Start Console Output Monitoring")
@@ -224,7 +55,7 @@ class LiveTableModel3(QWidget):
 
     # def console_monitoring_thread(self, *, callback):
     def console_monitoring_thread(self):
-        #print("Monitoring")
+        # print("Monitoring")
         for n in range(5):
             try:
                 msg = self.msg_queue.get(timeout=0.2)
@@ -250,12 +81,25 @@ class QtKafkaTableTab(QWidget):
         self.config = model.settings.gui_config
         bl_acronym = self.config.get("kafka", {}).get("bl_acronym", "")
         kafka_config = self.config.get("kafka", {}).get("config_file", "")
-        self.kafkaTable = LiveTableModel3(bl_acronym, kafka_config)
+        self.kafkaTable = LiveTableModel(bl_acronym, kafka_config)
         self.kafkaMonitor = QtReConsoleMonitor(self.kafkaTable, self)
+        # self.kafkaMonitor._text_edit.setFontFamily("monospace")
+
+        # Printing the font and font family used by self.kafkaMonitor
+        font = self.kafkaMonitor._text_edit.font()
+        font.setFamily("Monospace")
+        font.setStyleHint(QFont.Monospace)
+        self.kafkaMonitor._text_edit.setFont(font)
+
         vbox = QVBoxLayout()
         vbox.addWidget(QLabel("Kafka Table Monitor"))
         vbox.addWidget(self.kafkaMonitor)
         self.setLayout(vbox)
+
+        font = self.kafkaMonitor._text_edit.font()
+        actual_font = QFontInfo(font)
+        print(f"Font used: {actual_font.family()}, Font Desired: {font.family()}")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Kafka LiveTable Monitor")
@@ -277,13 +121,21 @@ def main():
     app = QApplication([])
 
     main_window = QMainWindow()
-    #model = LiveTableModel(args.bl, args.config_file, topic_string=args.topic_string)
-    #central_widget = LiveTableModel2(args.bl, args.config_file, topic_string=args.topic_string)
-    model = LiveTableModel3(args.bl, args.config_file, topic_string=args.topic_string)
-    #central_widget = LiveTableModel3(args.bl, args.config_file, topic_string=args.topic_string)
+    # model = LiveTableModel(args.bl, args.config_file, topic_string=args.topic_string)
+    # central_widget = LiveTableModel2(args.bl, args.config_file, topic_string=args.topic_string)
+    model = LiveTableModel(args.bl, args.config_file, topic_string=args.topic_string)
+    # central_widget = LiveTableModel3(args.bl, args.config_file, topic_string=args.topic_string)
     central_widget = QtReConsoleMonitor(model, main_window)
-    #model.setParent(central_widget)
-    #central_widget.start_console_output_monitoring()
+    # Ensure the font family is set to a monospace font that exists on the system
+    font = central_widget._text_edit.font()
+    font.setFamily("Monospace")
+    font.setStyleHint(QFont.Monospace)
+    central_widget._text_edit.setFont(font)
+    font = central_widget._text_edit.font()
+    actual_font = QFontInfo(font)
+    print(f"Font used: {actual_font.family()}, Font Desired: {font.family()}")
+    # model.setParent(central_widget)
+    # central_widget.start_console_output_monitoring()
     main_window.setCentralWidget(central_widget)
     central_widget.destroyed.connect(lambda: model.stop_console_output_monitoring)
     main_window.show()
